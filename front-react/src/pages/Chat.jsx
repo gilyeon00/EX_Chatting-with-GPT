a8b91b0a9060277d036a3562c9d86681eac6f6f9
gilyeon00 <12venus08@naver.com>
2023-07-11 15:53:30 +0900

    FE/Feat: 메세지 전송 화면 프레임

--- front-react/src/pages/Chat.jsx
+++ front-react/src/pages/Chat.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import './chat.css' 
+import React, { useState } from 'react';
+import './chat.css';
 
 const Chat = () => {
+    const [message, setMessage] = useState('');
+    const [chatHistory, setChatHistory] = useState([]);
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const handleSendMessage = () => {
+        if (message.trim() !== '') {
+            setChatHistory((prevChat) => [...prevChat, message]);
+            setMessage('');
+        }
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSendMessage();
+        }
+    };
+
     return (
         <div className='container'>
             <h1>1:1 채팅</h1>
             <div className="chat-container">
                 <div className='chat-history'>
-
+                    {chatHistory.map((msg, index) => (
+                        <div className="message-bubble" key={index}>
+                            {msg}
+                        </div>
+                    ))}
                 </div>
-                {/* 메시지 입력 및 전송 영역 */}
                 <div className="input-area">
-                    <input className="input" type="text" placeholder="메시지를 입력하세요" />
-                    <button>전송</button>
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="메시지를 입력하세요"
+                        value={message}
+                        onChange={handleMessageChange}
+                        onKeyDown={handleKeyDown}
+                    />
+                    <button onClick={handleSendMessage}>전송</button>
                 </div>
             </div>
         </div>
