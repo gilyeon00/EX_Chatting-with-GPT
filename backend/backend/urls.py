3d6b3b7382b7e6d6c124ee6ef1a3c694e1cc9d9a
gilyeon00 <12venus08@naver.com>
2023-07-11 14:44:10 +0900

    BE/Feat: chat 앱 생성

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from chat import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chat/', views.chat_view, name='chat'),
 ]
