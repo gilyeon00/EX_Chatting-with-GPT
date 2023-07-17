8bcd95866104750079987e73c68c9bfbb0044b9f
gilyeon00 <12venus08@naver.com>
2023-07-18 08:58:33 +0900

    del npm start

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,7 +56,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # ]
 
 # CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost', 'http://localhost:80', 'http://www.gilyeon.site')
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
 
