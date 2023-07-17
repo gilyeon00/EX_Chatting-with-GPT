ea9b62b4aed8cf1ce01bfc851b956bd8f2955bfd
gilyeon00 <12venus08@naver.com>
2023-07-18 07:35:12 +0900

    proxy 설정

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
 #     'http://gilyeon.site',
 # ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost', 'http://localhost:80', 'http://gilyeon.site')
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
 
