811b9da039c493e5e71414cff2f8da579cda3cf6
gilyeon00 <12venus08@naver.com>
2023-07-18 07:45:06 +0900

    add www

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 #     'http://gilyeon.site',
 # ]
 
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost', 'http://localhost:80', 'http://gilyeon.site')
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost', 'http://localhost:80', 'http://www.gilyeon.site')
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
